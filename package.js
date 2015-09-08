cc749fd48c95592fb72dad205bfe972257a5696f
Vu Tran <baysao@gmail.com>
2015-09-08 13:19:54 +0700

    first commit

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'dangrossman:bootstrap-daterangepicker',
+  name: 'baysao:bootstrap-daterangepicker',
   version: '2.0.9',
   summary: 'Date range picker component for Bootstrap',
   git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
@@ -8,8 +8,8 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
-
-  api.use('twbs:bootstrap@3.3.4', ["client"]);
+  api.use('nemo64:bootstrap@3.3.5_2', ["client"]);
+  api.use('nemo64:bootstrap-data@3.3.5', ["client"]);
   api.use('momentjs:moment@2.10.3', ["client"]);
   api.use('jquery@1.11.3_2', ["client"]);
 
