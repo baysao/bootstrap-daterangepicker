ebb9b038261ac2efdd1e76f22f74c394ee010e1e
Vu Tran <baysao@gmail.com>
2015-09-08 13:28:00 +0700

    first commit

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'baysao:bootstrap-daterangepicker',
   version: '2.0.9',
   summary: 'Date range picker component for Bootstrap',
-  git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
+  git: 'https://github.com/baysao/bootstrap-daterangepicker',
   documentation: 'README.md'
 });
 
